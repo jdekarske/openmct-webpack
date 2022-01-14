4b42133d1b68bac64e56492dd5723b014c9062dc
Jason Dekarske <dekarskej@gmail.com>
2022-01-13 17:27:11 -0800

    helloplugin works

---
+++ include/js/tracked-tabs-view.js
@@ -0,0 +1,116 @@
+// tabs vue component /home/guy/code/astrobee_internship/isaac_data_interface/frontend/openmct/src/plugins/tabs/components/tabs.vue
+// import Vue from 'vue';
+import TabsComponent from 'openmct/plugins/tabs/components/tabs.vue';
+
+// I want to show the tab but also log it.
+Vue.config.optionMergeStrategies.myOption = (toVal, fromVal) => {
+    return {
+        ...toVal,
+        ...fromVal
+    };
+};
+
+// define a component that uses this mixin
+var TrackedTabsComponent = Vue.extend({
+    mixins: [TabsComponent],
+    created: function () {
+        console.log("subscribe here")
+    },
+    methods: {
+        sendtoROS: function () {
+            console.log("sending")
+        }
+    }
+})
+
+
+// tabs view /home/guy/code/astrobee_internship/isaac_data_interface/frontend/openmct/src/plugins/tabs/tabs.js
+function TrackedTabs(openmct) {
+    return {
+        key: 'trackedtabs',
+        name: 'TrackedTabs',
+        cssClass: 'icon-list-view',
+        canView: function (domainObject) {
+            return domainObject.type === 'trackedtabs';
+        },
+        canEdit: function (domainObject) {
+            return domainObject.type === 'trackedtabs';
+        },
+        view: function (domainObject, objectPath) {
+            let component;
+
+            return {
+                show: function (element, editMode) {
+                    component = new Vue({
+                        el: element,
+                        components: {
+                            TrackedTabsComponent: TrackedTabsComponent.default
+                        },
+                        data() {
+                            return {
+                                isEditing: editMode
+                            };
+                        },
+                        provide: {
+                            openmct,
+                            domainObject,
+                            objectPath,
+                            composition: openmct.composition.get(domainObject)
+                        },
+                        template: '<tabs-component :isEditing="isEditing"></tabs-component>'
+                    });
+                },
+                onEditModeChange(editMode) {
+                    component.isEditing = editMode;
+                },
+                destroy: function (element) {
+                    component.$destroy();
+                    component = undefined;
+                }
+            };
+        },
+        priority: function () {
+            return 1;
+        }
+    };
+}
+
+// tabs plugin /home/guy/code/astrobee_internship/isaac_data_interface/frontend/openmct/src/plugins/tabs/plugin.js
+function TrackedTabsPlugin() {
+    return function install(openmct) {
+        openmct.objectViews.addProvider(new TrackedTabs(openmct));
+
+        openmct.types.addType('trackedtabs', {
+            name: "Tabs View",
+            description: 'based off the built-in tabs view, but will send a message to a rostopic when tabs are switched.',
+            creatable: true,
+            cssClass: 'icon-tabs-view',
+            initialize(domainObject) {
+                domainObject.composition = [];
+                domainObject.keep_alive = true;
+            },
+            form: [
+                {
+                    "key": "keep_alive",
+                    "name": "Eager Load Tabs",
+                    "control": "select",
+                    "options": [
+                        {
+                            'name': 'True',
+                            'value': true
+                        },
+                        {
+                            'name': 'False',
+                            'value': false
+                        }
+                    ],
+                    "required": true,
+                    "cssClass": "l-input"
+                }
+            ]
+        });
+
+    };
+}
+
+export default TrackedTabsPlugin;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// import openmct
 import openmct from 'MCT/openmct'
+import HelloPlugin from './include/js/HelloPlugin'
 
 const ONE_SECOND = 1000;
 const THIRTY_SECONDS = 30 * ONE_SECOND;
@@ -8,31 +8,31 @@ const THIRTY_MINUTES = 30 * ONE_MINUTE;
 
 
 openmct.setAssetPath("assets");
-// openmct.install(new HelloPlugin.default());
+openmct.install(new HelloPlugin());
 openmct.install(new openmct.plugins.LocalStorage());
 openmct.install(new openmct.plugins.Espresso());
 openmct.install(new openmct.plugins.MyItems());
 // openmct.install(new TrackedTabsPlugin())
 openmct.install(openmct.plugins.UTCTimeSystem());
 openmct.install(openmct.plugins.Conductor({
-      menuOptions: [
-          {
-              name: "Fixed",
-              timeSystem: 'utc',
-              bounds: {
-                  start: Date.now() - THIRTY_MINUTES,
-                  end: Date.now()
-              }
-  },
-          {
-              name: "Realtime",
-              timeSystem: 'utc',
-              clock: 'local',
-              clockOffsets: {
-                  start: - THIRTY_MINUTES,
-                  end: THIRTY_SECONDS
-              }
-          }
-      ]
-  }));
+    menuOptions: [
+        {
+            name: "Fixed",
+            timeSystem: 'utc',
+            bounds: {
+                start: Date.now() - THIRTY_MINUTES,
+                end: Date.now()
+            }
+        },
+        {
+            name: "Realtime",
+            timeSystem: 'utc',
+            clock: 'local',
+            clockOffsets: {
+                start: - THIRTY_MINUTES,
+                end: THIRTY_SECONDS
+            }
+        }
+    ]
+}));
 document.addEventListener('DOMContentLoaded', () => openmct.start(document.body));
